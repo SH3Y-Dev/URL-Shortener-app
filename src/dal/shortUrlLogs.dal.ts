8fd01d343faaf2a9ee1edf8bd6f0b79327657f95
S.Shreyas <65349480+shreyas-s19@users.noreply.github.com>
2024-11-24 16:40:16 +0530

    Storing Click counts & click by date

--- src/dal/shortUrlLogs.dal.ts
+++ src/dal/shortUrlLogs.dal.ts
@@ -15,13 +15,44 @@ export class ShortUrlLogsDal {
   async createShortUrlLogs(data): Promise<ShortUrlLogs> {
     try {
       const { shortUrl, ip, osName, deviceName } = data;
+
+      // Check if the IP already exists for the given shortUrl
+      const ipExists = await this.shortUrlLogs.findOne({
+        where: { shortUrl: { alias: shortUrl.alias }, ip },
+      });
+
+      console.log("ip exist",ipExists);
+      
+      // Create a new log entry
       const shortUrlLogs = this.shortUrlLogs.create({
         shortUrl,
         ip,
         osName,
         deviceName,
       });
-      return await this.shortUrlLogs.save(shortUrlLogs);
+      await this.shortUrlLogs.save(shortUrlLogs);
+
+      // Update totalClicks and uniqueClicks
+      if (!ipExists) {
+        // New IP: Increment both totalClicks and uniqueClicks
+        await this.shortUrl.update(
+          { alias: shortUrl.alias },
+          {
+            totalClicks: () => 'total_clicks + 1',
+            uniqueClicks: () => 'unique_clicks + 1',
+          },
+        );
+      } else {
+        // Existing IP: Increment only totalClicks
+        await this.shortUrl.update(
+          { alias: shortUrl.alias },
+          {
+            totalClicks: () => 'total_clicks + 1',
+          },
+        );
+      }
+
+      return shortUrlLogs;
     } catch (error) {
       throw new Error('Error saving short URL log');
     }
